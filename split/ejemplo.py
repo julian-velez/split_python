800acd0b5e5ea7d4f581b7c93edd825d2d698d46
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-07-06 10:04:41 -0500

    split_python
    split_python

---
+++ split/ejemplo.py
@@ -0,0 +1,10 @@
+# El método .split() en Python divide una cadena de texto en una lista de palabras, separándolas por defecto usando los espacios en blanco
+
+# Se crea una variable llamada 'curso' y se le asigna una cadena de texto
+curso = "cuantas horas le dedicas al dia a programar"
+
+# Se imprime el resultado de aplicar el método .split() sobre la cadena
+print(curso.split())
+
+
+
